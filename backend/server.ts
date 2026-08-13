19be74f09925fa4c6aeaeaa8ce81745ca721d3c0
Willy Hsu <N01678692@humber.ca>
2026-08-13 09:49:09 +0800

    removed slash

--- backend/server.ts
+++ backend/server.ts
@@ -15,7 +15,7 @@ const KATANA_BASE_URL = 'https://api.katanamrp.com/v1';
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174', "https://inventory.willyhsu0130.com/" ],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174', "https://inventory.willyhsu0130.com" ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
