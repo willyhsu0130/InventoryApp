7407db7d551e639a6a1ae3185543cb7fd5c0b0a9
Willy Hsu <N01678692@humber.ca>
2026-08-13 09:12:22 +0800

    changed to es imports for authRoutes

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,11 @@
 import type { Request, Response } from 'express';
-const dotenv = require('dotenv');
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const authRoutes = require('./authRoutes').default; // 👈 1. Import your auth router after env loads
+import authRoutes from "./authRoutes.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
