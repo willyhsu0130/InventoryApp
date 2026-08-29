f903ac3cbf5c7801a9aaa8c5e95e790bd57a2d77
Willy Hsu <N01678692@humber.ca>
2026-08-29 15:47:01 +0800

    removed lagacy manufacture features

--- apps/frontend/src/hooks/useContexts.tsx
+++ apps/frontend/src/hooks/useContexts.tsx
@@ -1,76 +1,11 @@
 import { useContext } from "react";
-import {
-    ProductContext
-
-} from "../context/product/ProductContext";
-
-import { InventoryContext } from "../context/inventory/InventoryContext";
-import { OrdersContext } from "@/context/orders/OrdersContext"
-import { CustomersContext } from "@/context/customers/CustomersContext";
-import { ManufactureContext } from "@/context/manufacture/ManufactureContext";
 import { AuthContext } from "@/context/auth/AuthContext";
-import { VariantContext } from "@/context/variant/VariantContext";
-
-export const useProductCatalog = () => {
-    const context = useContext(ProductContext);
-    if (!context) {
-        throw new Error("useProductCatalog must be used within a ProductProvider");
-    }
-    return context;
-};
-
-export const useInventoryCatalog = () => {
-    const context = useContext(InventoryContext);
-    if (!context) {
-        throw new Error("useInventoryCatalog must be used within a InventoryProvider");
-    }
-    return context;
-}
-
-export const useOrdersCatalog = () => {
-    const context = useContext(OrdersContext);
-    if (!context) {
-        throw new Error("useOrdersCatalog must be used within a OrdersProvider");
-    }
-    return context;
-}
-
-
-export const useCustomersCatalog = () => {
-    const context = useContext(CustomersContext);
-    if (!context) {
-        throw new Error("useCustomersCatalog must be used within a CustomerProvider");
-    }
-    return context;
-}
-
-export const useManufactureCatalog = () => {
-    const context = useContext(ManufactureContext);
-    if (!context) {
-        throw new Error(
-            "useManufacturingCatalog must be used within a ManufactureProvider"
-        );
-    }
-    return context;
-};
-
 
 export const useAuth = () => {
     const context = useContext(AuthContext);
     if (!context) {
         throw new Error(
             "useAuthCatalog must be used within a AuthProvider"
-        );
-    }
-    return context;
-};
-
-
-export const useVariant = () => {
-    const context = useContext(VariantContext);
-    if (!context) {
-        throw new Error(
-            "useVariantCatalog must be used within a VariantProvider"
         );
     }
     return context;
